bddb47a3b0d929c84bcae4d481426430679dcad4
Artur Maricato Curinga <arturmcuringa@gmail.com>
2017-06-09 00:22:38 -0300

    Add server side sockets

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -74,6 +74,108 @@ void MainWindow::updateLabels(void){
     ui->strs10->setText(QString::number(semaforo[9]->getContador()));
 }
 
+void MainWindow::serverHandler()
+{
+    struct sockaddr_in endereco;
+        int socketId;
+
+        //variáveis relacionadas com as conexões clientes
+        struct sockaddr_in enderecoCliente;
+        socklen_t tamanhoEnderecoCliente = sizeof(struct sockaddr);
+        int conexaoClienteId;
+
+        Operation op;
+        int byteslidos;
+
+        /*
+         * Configurações do endereço
+        */
+        memset(&endereco, 0, sizeof(endereco));
+        endereco.sin_family = AF_INET;
+        endereco.sin_port = htons(PORTNUM);
+        //endereco.sin_addr.s_addr = INADDR_ANY;
+        endereco.sin_addr.s_addr = inet_addr("192.168.7.1");
+
+        /*
+         * Criando o Socket
+         *
+         * PARAM1: AF_INET ou AF_INET6 (IPV4 ou IPV6)
+         * PARAM2: SOCK_STREAM ou SOCK_DGRAM
+         * PARAM3: protocolo (IP, UDP, TCP, etc). Valor 0 escolhe automaticamente
+        */
+        socketId = ::socket(AF_INET, SOCK_STREAM, 0);
+
+        //Verificar erros
+        if (socketId == -1)
+        {
+            printf("Falha ao executar socket()\n");
+            exit(EXIT_FAILURE);
+        }
+
+        //Conectando o socket a uma porta. Executado apenas no lado servidor
+        if ( ::bind (socketId, (struct sockaddr *)&endereco, sizeof(struct sockaddr)) == -1 )
+        {
+            printf("Falha ao executar bind()\n");
+            exit(EXIT_FAILURE);
+        }
+
+        //Habilitando o servidor a receber conexoes do cliente
+        if ( ::listen( socketId, 10 ) == -1)
+        {
+            printf("Falha ao executar listen()\n");
+            exit(EXIT_FAILURE);
+        }
+
+        //servidor ficar em um loop infinito
+        while(1)
+        {
+
+            conexaoClienteId = ::accept( socketId,(struct sockaddr *) &enderecoCliente,&tamanhoEnderecoCliente );
+
+            //Verificando erros
+            if ( conexaoClienteId == -1)
+            {
+                printf("Falha ao executar accept()");
+                exit(EXIT_FAILURE);
+            }
+
+            //receber uma msg do cliente
+            byteslidos = ::recv(conexaoClienteId,&op,sizeof(op),0);
+
+            if (byteslidos == -1)
+            {
+                printf("Falha ao executar recv()");
+                exit(EXIT_FAILURE);
+            }
+            else if (byteslidos == 0)
+            {
+                printf("Cliente finalizou a conexão\n");
+                exit(EXIT_SUCCESS);
+            }
+            if(op.op == 0)
+            {
+                for(int i = 0; i < 7; ++i)
+                {
+                    trem[i]->setEnable(true);
+                }
+            }
+            if(op.op == 1)
+            {
+                trem[op.id]->setEnable(true);
+            }
+            if(op.op == 2)
+            {
+                trem[op.id]->setEnable(false);
+            }
+            if(op.op == 3)
+            {
+                trem[op.id]->setVelocidade(op.value);
+            }
+            ::close(conexaoClienteId);
+        }
+
+}
+
 void MainWindow::updateInterface(int id, int x, int y)
 {
     updateLabels();
--- mainwindow.h
+++ mainwindow.h
@@ -4,7 +4,15 @@
 #include "trem.h"
 #include "semaforo.h"
 #include <QMainWindow>
+#include <cstdio>       //printf
+#include <cstring>      //memset
+#include <cstdlib>      //exit
+#include <netinet/in.h> //htons
+#include <arpa/inet.h>  //inet_addr
+#include <sys/socket.h> //socket
+#include <unistd.h>     //close
 
+#define PORTNUM 4555
 using namespace std;
 
 namespace Ui {
@@ -27,6 +35,14 @@ private:
     Ui::MainWindow *ui;
     Trem *trem[7];
     Semaforo* semaforo[10];
+    void serverHandler();
+
+    typedef struct{
+        int op;
+        int id;
+        int value;
+    }Operation;
+
 };
 
 #endif // MAINWINDOW_H
